f429505f98ede821c7b5c1d99a6a42b37cb1908c
César Jarith Coello <jarith182@gmail.com>
2024-01-25 13:10:27 -0600

    first commit

---
+++ 02-variables.py
@@ -0,0 +1,10 @@
+hola_mundo = 'Hola mundo'
+
+# Formas de escribir varibales
+nombreCliente = 'Cesar' #camelcase
+NombreCliente = 'Cesar' #Pascal Case o Capital Came case
+nombrecliente = 'Cesar' #flat case
+#nombre-cliente = 'Cesar' #Kebab Case o Dash Case
+nombre_cliente = 'Cesar' #Snake case
+
+print(hola_mundo)
---
+++ 03-tiposdedatos.py
@@ -0,0 +1,13 @@
+nombre = 'Cesar Coello'
+
+print(nombre)
+
+edad = 25
+edad = 20
+print(edad)
+
+cantidad_a_pagar = 100.5
+print(cantidad_a_pagar)
+
+pagado = True
+print(pagado)
