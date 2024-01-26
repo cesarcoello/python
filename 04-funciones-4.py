d3538447140506c7511311e672522ccac7d16694
César Jarith Coello <jarith182@gmail.com>
2024-01-26 11:23:31 -0600

    Diferencias entre funciones y metodos

---
+++ 04-funciones-4.py
@@ -0,0 +1,17 @@
+nombre = 'Cesar'
+
+def mostrar_nombre(nombre):
+    print(f'Hola {nombre}')
+
+mostrar_nombre(nombre)
+
+print( nombre.upper()) #Mayus
+print( nombre.title()) #Capitalizado
+
+
+usuario = 'Julian'
+
+def bienvenida(usuario):
+    print(f'Hola {usuario}, seas bienvenido!')
+
+bienvenida(usuario)
